87dca07c862790f12ad8d6a7b33f64dbedf9ed59
akrauss419 <akrauss419@gmail.com>
2023-01-08 15:50:55 -0800

    Updates

--- README.md
+++
@@ -1,6 +0,0 @@
-# Portfolio Website
-
-My portfolio website showcasing my projects. It also includes a basic profile, my resume, and contact info.
-
-# Preview
-<img src="https://i.imgur.com/2Gmwsyy.png">
---
+++ precache-manifest.debc36df8959f2241b4f7271caffcd73.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee9eaf0e21644574f56a",
+    "url": "/portfolio-website/static/css/main.6e13ab6f.chunk.css"
+  },
+  {
+    "revision": "ee9eaf0e21644574f56a",
+    "url": "/portfolio-website/static/js/main.ee9eaf0e.chunk.js"
+  },
+  {
+    "revision": "a7194b2d988a5c80f583",
+    "url": "/portfolio-website/static/js/1.a7194b2d.chunk.js"
+  },
+  {
+    "revision": "2b256896c0c48cf1d86c",
+    "url": "/portfolio-website/static/js/runtime~main.2b256896.js"
+  },
+  {
+    "revision": "a261b6ad17951737bc532a744d66a8df",
+    "url": "/portfolio-website/index.html"
+  }
+];
