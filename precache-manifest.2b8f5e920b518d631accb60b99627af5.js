512162d161b5c60d0ce7efb39da00cfd9ef8874a
akrauss419 <akrauss419@gmail.com>
2023-03-05 17:32:22 -0800

    Updates

---
+++ precache-manifest.2b8f5e920b518d631accb60b99627af5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f15a9487e0e09a151b04",
+    "url": "/portfolio-website/static/css/main.99b4108c.chunk.css"
+  },
+  {
+    "revision": "f15a9487e0e09a151b04",
+    "url": "/portfolio-website/static/js/main.f15a9487.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/portfolio-website/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "2b256896c0c48cf1d86c",
+    "url": "/portfolio-website/static/js/runtime~main.2b256896.js"
+  },
+  {
+    "revision": "6553cfbe8a727fd074497d5649b0f607",
+    "url": "/portfolio-website/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio-website/precache-manifest.0525c0fcd5e1391f33b16b48ad2fac30.js"
+  "/portfolio-website/precache-manifest.2b8f5e920b518d631accb60b99627af5.js"
 );
 
 workbox.clientsClaim();
